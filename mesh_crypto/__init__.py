896f8e2b715e193ba179923668026a9b26ddda72
ArtVotrex <feikq777@gmail.com>
2026-04-12 18:44:15 +0300

    feat(mesh_crypto): Завершена разработка тщательно отобранного общедоступного API верхнего уровня для mesh_crypto

---
+++ mesh_crypto/__init__.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from .core.keys import EncryptionKeyPair, SigningKeyPair
+from .core.key_types import KeyKind
+from .core.types import KeyId
+from .errors import (
+    AuthenticationError,
+    IntegrityError,
+    InvalidInputError,
+    InvalidKeyError,
+    KeyMismatchError,
+    KeyNotFoundError,
+    KeystoreError,
+    KeystoreNotLoadedError,
+    MalformedDataError,
+    MeshCryptoError,
+    ProtectorBackendUnavailableError,
+    ProtectorError,
+    ProtectorOperationError,
+    ReplayDetectedError,
+    RollbackDetectedError,
+    SignatureVerificationError,
+    UnsupportedFormatError,
+    WrongKeyTypeError,
+)
+from .keystore.file_keystore import FileKeyStore
+from .keystore.protectors import KeyringProtector, PasswordProtector
+from .primitives.aead import decrypt, encrypt
+from .primitives.dh import derive_session_key
+from .primitives.signatures import sign, verify
+
+__all__ = [
+    "KeyId",
+    "KeyKind",
+    "SigningKeyPair",
+    "EncryptionKeyPair",
+    "sign",
+    "verify",
+    "encrypt",
+    "decrypt",
+    "derive_session_key",
+    "FileKeyStore",
+    "PasswordProtector",
+    "KeyringProtector",
+    "MeshCryptoError",
+    "InvalidInputError",
+    "InvalidKeyError",
+    "WrongKeyTypeError",
+    "KeyMismatchError",
+    "UnsupportedFormatError",
+    "MalformedDataError",
+    "IntegrityError",
+    "AuthenticationError",
+    "SignatureVerificationError",
+    "ReplayDetectedError",
+    "RollbackDetectedError",
+    "KeystoreError",
+    "ProtectorError",
+    "ProtectorBackendUnavailableError",
+    "ProtectorOperationError",
+    "KeyNotFoundError",
+    "KeystoreNotLoadedError",
+]
