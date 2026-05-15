8b588b80ee3fe4245bcde7c3f628b2c2966abd68
ArtVotrex <feikq777@gmail.com>
2026-05-15 18:27:51 +0300

    fix(mesh_node_db): бля забыл __init__ поменять

--- mesh_crypto/keystore/__init__.py
+++ mesh_crypto/keystore/__init__.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 
 from .file_keystore import FileKeyStore
 from .protectors import KeyringProtector, PasswordProtector, Protector
-from .operations import sign_with_key
+from .operations import sign_with_key, require_signing_key_matches_public_key
 
 __all__ = [
     "Protector",
     "PasswordProtector",
     "KeyringProtector",
     "FileKeyStore",
-    "sign_with_key"
+    "sign_with_key",
+    "require_signing_key_matches_public_key"
 ]
 
-__version__ = "0.36.0"
+__version__ = "0.37.0"
